b9758104fc92be93b5c767a6e53d80cb73d94a1c
TroyFletcher <fletcher.troy@gmail.com>
2023-11-29 12:57:07 -0500

    Many more useful functions for keyboard design

--- best_guess.py
+++ best_guess.py
@@ -92,3 +92,111 @@ def best_guess(origin_mod_ref, target_mod_ref, angle_degrees, distance_mm, orien
     target_mod.SetPosition(pcbnew.VECTOR2I(int(newx),int(newy)))
     if orient: target_mod.SetOrientation(origin_orientation)
     pcbnew.Refresh();
+
+def manual_move(mod_ref, x_move, y_move):
+    """
+    Move module a fixed distance from itself
+    EXAMPLE: manual_move("K56", -(UNIT/3), UNIT)
+    EXPLANATION: Move K56 from current x,y to new x,y one third of UNIT left, and UNIT down
+    NOTE: If using this function in a loop, comment out the refresh() from the end as it is
+          expensive in time. Better to rsh() manually after the looping is complete.
+    """
+    module = board.FindFootprintByReference(mod_ref)
+    startx,starty = module.GetPosition().x,module.GetPosition().y
+    newx = startx + (x_move * SCALE)
+    newy = starty + (y_move * SCALE)
+    module.SetPosition(pcbnew.VECTOR2I(int(newx),int(newy)))
+    pcbnew.Refresh();
+
+def align_column_from_top_down(column, angle_degrees):
+    """
+    Use best_guess to align 
+    EXAMPLE: align_column_from_top_down(["K12","K24","K36","K64","K58"], 10)
+    EXPLANATION: Based on the position of K12, run best_guess on K24 with 1 unit spacing
+                 increasing unit spacing by 1 each entry in the list
+    """
+    origin = column.pop(0)
+    spacing = 1
+    for target in column:
+        best_guess(origin, target, angle_degrees, UNIT*spacing, ORIENT)
+        spacing+=1
+    column = column.insert(0,origin)
+
+def consecutive_module_mover(home_module_ref, name_prefix, num_begin, num_end, move_dir_x, move_dir_y):
+    """
+    Move consecutive modules matching a naming convention of modules consecutively in x,y direction
+    Place key footprints in a row at fixed interval
+    EXAMPLE: consecutive_module_mover("K18", "K", 19, 29, UNIT, 0)
+    EXPLANATION: From K18, move modules named "K" numbered 19 through 29 1 UNIT per module right, and 0 UNITs down
+                 This places switches K18 through K29 in a horizontal row 1 UNIT apart.
+    """
+    home_module = board.FindFootprintByReference(home_module_ref)
+    for next_module_num in range(num_begin,num_end+1):
+        next_module_ref = name_prefix + str(next_module_num)
+        next_module = board.FindFootprintByReference(next_module_ref)
+        startx,starty = home_module.GetPosition().x,home_module.GetPosition().y
+        newx = startx + (move_dir_x * SCALE)
+        newy = starty + (move_dir_y * SCALE)
+        next_module.SetPosition(pcbnew.VECTOR2I(int(newx),int(newy)))
+        home_module = next_module
+
+def diode_displacer(origin_name_prefix, target_name_prefix, num_begin, num_end, move_dir_x, move_dir_y):
+    """
+    Place footprints at XY offset based on matching number module reference of alternate prefix
+    Place matching diodes near their keyswitches
+    EXAMPLE: diode_displacer("K", "D", 1, 91, 0, 9.525)
+    EXPLANATION: For footprints matching K1 through K91, place D1 through D91 at the same X coordinate
+                 as the K module, and 9.525 lower than the Y coordinate of the K module
+    """
+    for module_num in range(num_begin,num_end+1):
+        origin_module_ref = origin_name_prefix + str(module_num)
+        target_module_ref = target_name_prefix + str(module_num)
+        origin_module = board.FindFootprintByReference(origin_module_ref)
+        target_module = board.FindFootprintByReference(target_module_ref)
+        startx,starty = origin_module.GetPosition().x,origin_module.GetPosition().y
+        newx = startx + (move_dir_x * SCALE)
+        newy = starty + (move_dir_y * SCALE)
+        target_module.SetPosition(pcbnew.VECTOR2I(int(newx),int(newy)))
+
+def diode_tracer(layer, switch_name_prefix, num_begin, num_end, startx, starty, endx, endy):
+    """
+    This isn't for diodes, it places copies of trace segments around each module of prefix
+    allowing you to _stamp_ traces around each instance of modules of a certain prefix.
+    Layer reference is the layer object; use pcbnew.F_Cu or pcbnew.B_Cu (NOT strings)
+    Switch name prefix is what preceeds the module id number (usually "K")
+    num begin/end are module numbers to start increment till end (inclusive)
+    startx/y are trace start position AS DELTA OF switch origin. Press space on switch origin,
+      then point to the start pad, and record the dx and dy
+    end x/y are same as start, delta of switch origin where the trace segment ends
+    Trace will be a straight line between these two points. Build complex traces as segments
+    EXAMPLE: diode_tracer(pcbnew.F_Cu, "K", 1, 91, -3.81, -2.54, -3.81, 9.525)
+    EXPLANATION: Starting from K1's footprint position (K1x,K1y) create a trace segment
+                 beginning at point (K1x+-3.81,K1y+-2.54) and ending at (K1x+-3.81,K1y+9.525)
+                 Continue making copies of this trace based on the footprints of K2-K91.
+    """
+    board = pcbnew.GetBoard()
+    for module_num in range(num_begin,num_end+1):
+        switch_module = board.FindFootprintByReference(switch_name_prefix + str(module_num))
+        switchx,switchy = switch_module.GetPosition().x,switch_module.GetPosition().y
+        track = pcbnew.PCB_TRACK(board)
+        track.SetStart(pcbnew.VECTOR2I(switchx + int(startx * SCALE),switchy + int(starty * SCALE)))
+        track.SetEnd(pcbnew.VECTOR2I(switchx + int(endx * SCALE),switchy + int(endy * SCALE)))
+        track.SetWidth(int(0.25 * SCALE))
+        track.SetLayer(layer)
+        board.Add(track)
+
+def add_track(startx, starty, endx, endy, layer):
+    """
+    Add a single track programatically. Best used with a metacode generator.
+    Technically, this can add any graphical line segment to an appropriate layer
+    Layer reference is the layer object; use pcbnew.F_Cu or pcbnew.B_Cu (NOT strings)
+    EXAMPLE: add_track(101.25, 19.05, 101.25, 38.1 pcbnew.F_Cu)
+    EXPLANATION: Create a track segment on the front copper layer.
+    """
+    board = pcbnew.GetBoard()
+    track = pcbnew.PCB_TRACK(board)
+    track.SetStart(pcbnew.VECTOR2I(int(startx * SCALE),int(starty * SCALE)))
+    track.SetEnd(pcbnew.VECTOR2I(int(endx * SCALE),int(endy * SCALE)))
+    track.SetWidth(int(0.25 * scale))
+    track.SetLayer(layer)
+    board.Add(track)
